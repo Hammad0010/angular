0183412017568f2e1a9dc4252bea56a2936f41c3
unknown <hammadmusawar@gmail.com>
2022-01-02 14:51:52 +0500

    deploy angular app to heroku demo code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,70 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IsbComponent } from './isb/isb.component';
+import { KarComponent } from './kar/kar.component';
+import { ExploreComponent } from './explore/explore.component';
+import { LahoreComponent } from './lahore/lahore.component';
+import { QuettaComponent } from './quetta/quetta.component';
+import { KashmirComponent } from './kashmir/kashmir.component';
+import { GilgitComponent } from './gilgit/gilgit.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { NewComponent } from './new/new.component';
+
+
+
+
+const routes: Routes = [
+ 
+  {
+    path:'no',
+    component:NewComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'gilgit',
+    component:GilgitComponent
+  },
+  {
+    path:'islamabad',
+    component:IsbComponent
+  },
+  {
+    path:'karachi',
+    component:KarComponent
+  },
+  {
+    path:'explore',
+    component:ExploreComponent
+  },
+  {
+    path:'lahore',
+    component:LahoreComponent
+  },
+  {
+    path:'quetta',
+    component:QuettaComponent
+  },
+  {
+    path:'kashmir',
+    component:KashmirComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IsbComponent } from './isb/isb.component';
+import { KarComponent } from './kar/kar.component';
+import { HomeComponent } from './home/home.component';
+import { ExploreComponent } from './explore/explore.component';
+import { LahoreComponent } from './lahore/lahore.component';
+import { QuettaComponent } from './quetta/quetta.component';
+import { GilgitComponent } from './gilgit/gilgit.component';
+import { KashmirComponent } from './kashmir/kashmir.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewComponent } from './new/new.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IsbComponent,
+    KarComponent,
+    HomeComponent,
+    ExploreComponent,
+    LahoreComponent,
+    QuettaComponent,
+    GilgitComponent,
+    KashmirComponent,
+    AboutComponent,
+    ContactComponent,
+    NewComponent,
+  
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
